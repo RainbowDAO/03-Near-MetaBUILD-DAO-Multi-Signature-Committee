da9ef341d08dc4f501cf62bffe88fed53e8509c0
HarrisWongg <harriswong@yeah.net>
2022-02-15 16:05:46 +0800

    add contract

--- Solidity-version-contract-for-Aurora/README.md
+++ Solidity-version-contract-for-Aurora/README.md
@@ -1,1 +1,47 @@
-Solidity-version-contract-for-Aurora
+# Getting Started
+## **Dependencies**
+
+- Linux , Mac or Windows
+- node ≥ 14
+- truffle v5.4.22
+## test-net-contract
+
+test-network:aurora
+
+### address
+
+#### DaoFactory
+0x829C1A191f33C02a92711665c4348c7d933E757A
+#### ERC20Factory
+0x20613AbF20ab1Fb3eE2200471E02A6fafCbddc84
+
+
+
+## **Installing**
+
+```bash
+git clone https://github.com/RainbowDAO/03-Near-MetaBUILD-DAO-Multi-Signature-Committee.git
+cd 03-Near-MetaBUILD-DAO-Multi-Signature-Committee/
+npm install
+```
+
+## **Prepare some ETH**
+You may need 1 ~ 3 ETH in your wallet.
+
+
+## **Compile  Contracts**
+```cd Solidity-version-contract-for-Aurora```
+
+```truffle compile```
+## **Deploy  Contracts**
+
+
+You will deploy 2 factory contracts
+- ERC20Factory
+- DaoFactory
+
+To deploy the contracts,First write your private key in the .secret ,second open truffle-config.js . Then change the defaultNetwork accordingly. Then run ```truffle migrate --network mainnet```
+,or deploy on your own network,run ```truffle migrate --network your network```
+
+
+
---
+++ Solidity-version-contract-for-Aurora/contracts/DaoFactory.sol
@@ -0,0 +1,60 @@
+pragma solidity ^0.8.0;
+import './GovernanceDao.sol';
+
+import {PublicStructs} from './lib/PublicStructs.sol';
+
+contract DaoFactory {
+       using EnumerableSet for EnumerableSet.AddressSet;
+
+    struct userData {
+        EnumerableSet.AddressSet daos;
+    }
+    struct daoInfo {
+        string name;
+        address dao;
+    }
+    mapping(address => userData) private _userDaos;
+    daoInfo[] public daos;
+    mapping(address => address[]) public daoUsers;
+    function createDao(uint parliamentCount, uint approveCount, uint workDays, uint applyTime, uint applyMemberCount, uint depositCount, uint voteInterval, uint publicityTime, string memory name,address govToken
+    ) public returns(address){
+        PublicStructs.InitGovernance memory pi = PublicStructs.InitGovernance({
+            parliamentCount:parliamentCount,
+            approveCount:approveCount,
+            workDays:workDays,
+            applyTime:applyTime,
+            applyMemberCount:applyMemberCount,
+            depositCount:depositCount,
+            voteInterval:voteInterval,
+            publicityTime:publicityTime,
+            name:name
+        });
+        GovernanceDao dao = new GovernanceDao(govToken,pi);
+        daoInfo memory df = daoInfo({name:name,dao:address(dao)});
+        daos.push(df);
+        _userDaos[msg.sender].daos.add(address(dao));
+        daoUsers[address(dao)].push(msg.sender);
+        return (address(dao));
+    }
+    function getDaoCount() public view returns(uint) {
+        return daos.length;
+    }
+    function joinDao(address daoAddress) public {
+        require(hasDao(msg.sender,daoAddress) == false,'error:you already join this dao');
+        _userDaos[msg.sender].daos.add(daoAddress);
+        daoUsers[daoAddress].push(msg.sender);
+    }
+    function getDaoUserLength(address daoAddress) public view returns(uint){
+        return daoUsers[daoAddress].length;
+    }
+    function hasDao(address user,address dao) public  view returns (bool){
+        return _userDaos[user].daos.contains(dao);
+    }
+
+    function getUserDaoLength(address user) public view returns(uint)  {
+        return _userDaos[user].daos.length();
+    }
+    function getUserDaoByIndex(address user , uint index) public view returns(address) {
+        return _userDaos[user].daos.at(index);
+    }
+}
